546b548b0724dd3597dea47d0149fa16dfb81b4f
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-07 11:57:06 +0300

    Change ScheduleController

--- src/main/java/com/javaschool/schedule/controller/ScheduleController.java
+++ src/main/java/com/javaschool/schedule/controller/ScheduleController.java
@@ -2,16 +2,13 @@ package com.javaschool.schedule.controller;
 
 import com.javaschool.schedule.Consumer;
 import com.javaschool.schedule.model.TherapyCase;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/schedule")
 public class ScheduleController {
 
     @ModelAttribute("cases")
@@ -19,7 +16,7 @@ public class ScheduleController {
         return Consumer.list;
     }
 
-    @GetMapping("/1")
+    @GetMapping("")
     public String schedule(){
         return "index";
     }
