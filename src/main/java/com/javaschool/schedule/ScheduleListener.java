d12971c5ba0c0f4640545d5d93291d65ad87d04e
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-09 10:56:09 +0300

    Add ScheduleListener.java

---
+++ src/main/java/com/javaschool/schedule/ScheduleListener.java
@@ -0,0 +1,42 @@
+package com.javaschool.schedule;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.javaschool.schedule.config.JMSConfig;
+import com.javaschool.schedule.model.TherapyCase;
+import com.javaschool.schedule.service.ScheduleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+@Slf4j
+public class ScheduleListener {
+
+    public static List<TherapyCase> list;
+
+    private final JMSConfig receiverConfigBean;
+    private final ScheduleService service;
+
+    @Autowired
+    public ScheduleListener(JMSConfig receiverConfigBean, ScheduleService service){
+        this.receiverConfigBean = receiverConfigBean;
+        this.service = service;
+    }
+
+    @JmsListener(destination = "${activemq.destination}", containerFactory = "jmsFactory")
+    public void processMessage(String response) throws JsonProcessingException {
+        list = service.getSchedule(response);
+        log.info("Schedule update > " + list);
+    }
+
+    @PostConstruct
+    public void init() throws JsonProcessingException {
+        receiverConfigBean.connectionFactory();
+        list = service.getSchedule();
+        log.info("Schedule init  > " + list);
+    }
+}
