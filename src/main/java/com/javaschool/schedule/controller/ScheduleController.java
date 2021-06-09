b33e78490eb6f12624d8f9f83285ea17eab6153c
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-09 10:55:54 +0300

    Add model attribute ScheduleListener.list to ScheduleController

--- src/main/java/com/javaschool/schedule/controller/ScheduleController.java
+++ src/main/java/com/javaschool/schedule/controller/ScheduleController.java
@@ -1,6 +1,6 @@
 package com.javaschool.schedule.controller;
 
-import com.javaschool.schedule.Consumer;
+import com.javaschool.schedule.ScheduleListener;
 import com.javaschool.schedule.model.TherapyCase;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +13,7 @@ public class ScheduleController {
 
     @ModelAttribute("cases")
     public List<TherapyCase> therapyCase() {
-        return Consumer.list;
+        return ScheduleListener.list;
     }
 
     @GetMapping("")
