1a51070d1ca4e75ea152ee0d7179444384bc9a2c
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-17 09:35:14 +0300

    Add  "SimpMessagingTemplate" to ScheduleListener.java (websocket)

--- src/main/java/com/javaschool/schedule/ScheduleListener.java
+++ src/main/java/com/javaschool/schedule/ScheduleListener.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.javaschool.schedule.config.JMSConfig;
 import com.javaschool.schedule.model.TherapyCase;
 import com.javaschool.schedule.service.ScheduleService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -22,6 +23,9 @@ public class ScheduleListener {
     private final ScheduleService service;
 
     @Autowired
+    private SimpMessagingTemplate messageTemplate;
+
+    @Autowired
     public ScheduleListener(JMSConfig receiverConfigBean, ScheduleService service){
         this.receiverConfigBean = receiverConfigBean;
         this.service = service;
@@ -30,6 +34,7 @@ public class ScheduleListener {
     @JmsListener(destination = "${activemq.destination}", containerFactory = "jmsFactory")
     public void updateSchedule(String response) throws JsonProcessingException {
         list = service.getSchedule(response);
+        messageTemplate.convertAndSend("/room.2","Update");
         log.info("Schedule update > " + list);
     }
 
