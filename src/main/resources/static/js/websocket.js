8b95d13c38e4a464b76f229c3a3833590ee64928
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-06-17 10:22:19 +0300

    Refactor websocket.js

--- src/main/resources/static/js/websocket.js
+++ src/main/resources/static/js/websocket.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    var messageList = $("#messages"); // defined a connection to a new socket endpoint
     var socket = new SockJS('/schedule');
     var stompClient = Stomp.over(socket);
 
-    stompClient.connect({ }, function(frame) {
-        stompClient.subscribe("/room.2", function(data) { // subscribe to the /topic/message endpoint
-            var message = data.body;
+    stompClient.connect({ }, function() {
+        stompClient.subscribe("/message", function() { // subscribe to the /schedule/message endpoint
             window.location.reload();
         });
     });
